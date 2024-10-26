762495a27991f9a0295f30d7731cde230996af54
Esmanuel P. Alayo <epiletaalayo@gmail.com>
2024-10-26 17:00:22 -0300

    Update Ejercico_5.py

--- Ejercicios de Alternativas/Ejercico_5.py
+++ Ejercicios de Alternativas/Ejercico_5.py
@@ -2,7 +2,7 @@
 ano=int(input("Digite un año para saber si es bisiesto o no"))
 
 def es_bisiesto(ano):
-    if(ano%4==0 and ano%100 !=0 or ano%400==0):
+    if(ano%4==0 and ano !=100 or ano%400==0):
         return True
     else:
         return False
